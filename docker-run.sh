a7395158426b33993c9e6c52ac81d5091e629f2b
Son Dinh <tsondt@gmail.com>
2018-02-11 00:17:45 -0500

    Enable previewing drafts

--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker run -ti --rm -v $PWD:/srv -p 4000:4000 "tsondt/blog"
+docker run -ti --rm -v $PWD:/srv -p 4000:4000 "tsondt/blog" check ||
+docker run -ti --rm -v $PWD:/srv -p 4000:4000 "tsondt/blog" install &&
+docker run -ti --rm -v $PWD:/srv -p 4000:4000 "tsondt/blog" exec jekyll serve --host 0.0.0.0 --port 4000 $1
