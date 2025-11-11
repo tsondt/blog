abfc1ee0d97d383c77e3cb0cd11e0b3490c85acb
Son Dinh <tsondt@gmail.com>
2025-11-10 21:30:25 -0500

    Updates

--- docker-run.sh
+++ docker-run.sh
@@ -1,5 +1,25 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-docker run -ti --rm -v $PWD:/srv -p 4000:4000 "tsondt/blog" check ||
-docker run -ti --rm -v $PWD:/srv -p 4000:4000 "tsondt/blog" install &&
-docker run -ti --rm -v $PWD:/srv -p 4000:4000 "tsondt/blog" exec jekyll serve --host 0.0.0.0 --port 4000 $1
+CONTAINER_NAME="tsondt/blog"
+CONTAINER_PORT=4000
+ARGS="${CONTAINER_PORT} "$@"
+
+usage () {
+    echo "Usage:"
+    echo "\t./docker-run.sh"
+    echo "\t./docker-run.sh [port] [jekyll serve extra args]"
+}
+
+if ! [ $# -eq 0 ]; then
+    re='^[0-9]+$'
+    if ! [[ $1 =~ $re ]] ; then
+        usage
+        exit 1
+    fi
+    CONTAINER_PORT=$1
+    ARGS="$@"
+fi
+
+docker run --rm -v $PWD:/srv "${CONTAINER_NAME}" check ||
+docker run --rm -v $PWD:/srv "${CONTAINER_NAME}" install &&
+docker run -ti --rm -v $PWD:/srv -p ${CONTAINER_PORT}:${CONTAINER_PORT} "${CONTAINER_NAME}" exec jekyll serve --host 0.0.0.0 --port ${ARGS}
